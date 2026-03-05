396f52fb9a05433db1c1f633b9d53c71b5ecfcf5
Andres <andresfdez256@gmail.com>
2026-03-05 09:06:13 +0100

    Clase ArmaCuerpoACuerpo finalizada

---
+++ src/Armas/ArmaCuerpoACuerpo.java
@@ -0,0 +1,24 @@
+package Armas;
+
+import Personajes.Personaje;
+
+public class ArmaCuerpoACuerpo extends Arma{
+
+	public ArmaCuerpoACuerpo(String nombre, int dañoBase, double probCritico, double multiplicadorCritico) {
+		super(nombre, dañoBase, probCritico, multiplicadorCritico);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int calcularDaño(Personaje atacante, Personaje defensor) {
+		// TODO Auto-generated method stub
+		 int daño = dañoBase + (int)(atacante.getAtaqueBase() * 1.3);
+	        if (esCritico()) {
+	            daño = (int)(daño * multiplicadorCritico);
+	            System.out.println("  [CRITICO!]");
+	        }
+	        return daño;
+	    }
+	@Override public String descripcion() { return "[CaC] " + super.descripcion(); }
+	
+}
